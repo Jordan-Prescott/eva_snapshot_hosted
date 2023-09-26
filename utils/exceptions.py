06a5745236ee3b8d48264970bb2f1a5cd3990801
Jordan Prescott <jprescott23@gmail.com>
2023-09-26 10:48:47 +0100

    added API call failure exception.

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -5,3 +5,18 @@ class EVASHError(Exception):
     def __str__(self) -> str:
         return "An error occurred in the EVA Snapshot Hosted module. \
             Please check your configuration and try again."
+
+class EVASHApiCallFail(EVASHError):
+    """ API call to download project failed.
+    """
+
+    def __str__(self) -> str:
+        return f"Failed to download project export in API. Please check URL, token,\
+             project_id, and account_id."
+
+class EVASHFilesNotLoaded(EVASHError):
+    """ Core files needed to generate output have not been found.
+    """
+
+    def __str__(self) -> str:
+        return f"Failed to load core files. Please check the name and path in Poly platform."
