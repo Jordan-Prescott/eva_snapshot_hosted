4d7b102b09dc25623a772212738774dc7fc448bf
Jordan Prescott <jprescott23@gmail.com>
2023-10-03 15:48:35 +0100

    get request method name update

--- evash_django_api/api/views.py
+++ evash_django_api/api/views.py
@@ -2,6 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
-def getData(request):
+def generate_snapshot(request):
     person = {'name': 'Jordan', 'age': '25'}
     return Response(person)
