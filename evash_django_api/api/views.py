a2f596b403e98238e5f3bf5c7e1acb7d390587fc
Jordan Prescott <jprescott23@gmail.com>
2023-10-06 16:01:45 +0100

    return image testing in progress

--- evash_django_api/api/views.py
+++ evash_django_api/api/views.py
@@ -1,9 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import FileResponse
 
 from eva_snapshot.utils.tests import return_string
 
+import os
+
 @api_view(['GET'])
 def getData(request):
-    string = return_string("This is my string")
-    return Response(string)
+    
+       # Define the path to the image file
+    image_path = 'eva.jpg' # Adjust the file path as needed
+
+    with open(image_path, 'rb') as image_file:
+        return FileResponse(image_file)
