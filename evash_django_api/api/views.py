dd055ccf578a7b103be206bd7ed688811a5ecea5
Jordan Prescott <jprescott23@gmail.com>
2023-10-17 13:45:49 +0100

    api now returns a zip file with an image inside

--- evash_django_api/api/views.py
+++ evash_django_api/api/views.py
@@ -1,16 +1,34 @@
-from rest_framework.response import Response
+from io import BytesIO
 from rest_framework.decorators import api_view
-from django.http import FileResponse
-
-from eva_snapshot.utils.tests import return_string
+from django.http import HttpResponse
+import zipfile
 
 import os
 
-@api_view(['GET'])
-def getData(request):
-    
-       # Define the path to the image file
-    image_path = 'eva.jpg' # Adjust the file path as needed
+#TODO: Need a new view for authentication
 
-    with open(image_path, 'rb') as image_file:
-        return FileResponse(image_file)
+@api_view(['GET'])
+def getData(token, group_id):
+    """ ...
+
+    :param token: 
+    :param group_id:
+    """
+    # this will return a zip file with snapshot output.
+
+
+    buffer = BytesIO()
+
+    with zipfile.ZipFile(buffer, 'w') as zipf:
+        # Add files to the zip archive, for example:
+        zipf.write('./eva.jpg', 'eva.jpg')
+
+    buffer.seek(0)
+
+    # Create an HttpResponse with the zip file as content
+    response = HttpResponse(buffer.read(), content_type='application/zip')
+    
+    # Set the Content-Disposition header to prompt the user to download the file
+    response['Content-Disposition'] = 'attachment; filename="eva.zip"'
+    
+    return response
