19de80c17b9c929be839d4375cba2ecace34bb37
Jordan Prescott <jprescott23@gmail.com>
2023-10-17 14:01:20 +0100

    API now returns evasnapshot output

--- evash_django_api/api/views.py
+++ evash_django_api/api/views.py
@@ -8,21 +8,35 @@ import os
 #TODO: Need a new view for authentication
 
 @api_view(['GET'])
-def getData(token, group_id):
+def getData(request):
     """ ...
+
+    This should take a token and a group id.
 
     :param token: 
     :param group_id:
     """
     # this will return a zip file with snapshot output.
 
+    
+    """
+    AUTH first of course
+
+    1. Run EVASnapshot
+    2. Collect files and zip
+    3. Send 
+    """
+
 
     buffer = BytesIO()
+    folder_path = './eva_snapshot/output/'
+    files = os.listdir(folder_path)
 
     with zipfile.ZipFile(buffer, 'w') as zipf:
-        # Add files to the zip archive, for example:
-        zipf.write('./eva.jpg', 'eva.jpg')
-
+        for file in files:
+            if os.path.isfile(os.path.join(folder_path, file)):
+                zipf.write(os.path.join(folder_path, file), file)
+ 
     buffer.seek(0)
 
     # Create an HttpResponse with the zip file as content
