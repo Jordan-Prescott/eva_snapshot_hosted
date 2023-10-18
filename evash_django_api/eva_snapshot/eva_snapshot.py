d81b04967b948aed12714e7a52d0b1e5ebac9206
Jordan Prescott <jprescott23@gmail.com>
2023-10-18 14:06:51 +0100

    script structure change for api call

--- evash_django_api/eva_snapshot/eva_snapshot.py
+++ evash_django_api/eva_snapshot/eva_snapshot.py
@@ -32,9 +32,36 @@ FILES_NOT_NEEDED = [
 ]
 
 
-def main():
-    """main method that manages user input and orchestrates programs operations.
+def main(account_id, project_id, group_id, region):
     """
+    company: FourteenIP Communications
+    author: Jordan Prescott
+    support: aisupport@fourteenip.com
+    
+    gitHub: https://github.com/Jordan-Prescott/eva_snapshot_hosted
+    
+    
+    main method that manages user input and orchestrates programs operations.
+    """
+
+    # setup logging
+    logger = logging.getLogger("EVASnapshot v2")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler = SysLogHandler(address=(PAPERTRAIL[0], PAPERTRAIL[1]))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    
+    logger.info(f"CUSTOMER_EMAIL: {CUSTOMER_EMAIL}, SITE_CODE: {SITE_CODE}, ACCOUNT ID: {ACCOUNT_ID}, PROJECT ID: {PROJECT_ID}")
+   
+
+    CORE_FILES.append(f"./input/project_download/data_store/live/{group_id.lower()}/handoff")
+    CORE_FILES.append(f"./input/project_download/data_store/live/{group_id.lower()}/sms_content_map")
+    CORE_FILES.append("./input/project_download/agent_configuration/domain/variants.yaml")
+    CORE_FILES.append("./input/project_download/agent_configuration/domain/utterances.en-US.yaml")
+
     api = PolyApi(API_URL, API_TOKEN, ACCOUNT_ID, PROJECT_ID)
     download = api.download_project()
     files.unpack_project_download(download, PROJECT_FOLDER, PROJECT_DOWNLOAD_NAME)
@@ -80,77 +107,3 @@ def main():
     uni.build_universe(f"{OUTPUT_FOLDER}{SITE_CODE}/")
 
     files.remove_folder(PROJECT_FOLDER)
-
-if __name__ == '__main__':
-    """
-    company: FourteenIP Communications
-    author: Jordan Prescott
-    support: aisupport@fourteenip.com
-    
-    gitHub: https://github.com/Jordan-Prescott/eva_snapshot_hosted
-    """
-
-    parser = argparse.ArgumentParser(
-        description="""Generate visual call flows for ACCOUNT_ID and PROJECT_ID passed in.
-        Stores USERNAME and SITE_CODE for log."""
-    )
-    parser.add_argument(
-        "--CUSTOMER_EMAIL",
-        help="USERNAME for log",
-        required=False
-    )
-    parser.add_argument(
-        "--SITE_CODE",
-        help="target site code for output",
-        required=False
-    )
-    parser.add_argument(
-        "--ACCOUNT_ID",
-        help="account id site is hosted under in PolyAI platform",
-        required=False
-    )
-    parser.add_argument(
-        "--PROJECT_ID",
-        help="project id of the project in PolyAI platform",
-        required=False
-    )
-    parser.add_argument(
-        "--API_URL",
-        help="Base url of API to download project backup.",
-        required=False
-    ) 
-    parser.add_argument(
-        "--API_TOKEN",
-        help="API token needed to authenticate and request project.",
-        required=False
-    )
-    
-    # capture arguments passed in
-    args = parser.parse_args()
-    CUSTOMER_EMAIL = args.CUSTOMER_EMAIL
-    SITE_CODE = args.SITE_CODE
-    ACCOUNT_ID = args.ACCOUNT_ID
-    PROJECT_ID = args.PROJECT_ID
-    API_URL = args.API_URL
-    API_TOKEN = args.API_TOKEN
-
-    # populate core files
-    CORE_FILES.append(f"./input/project_download/data_store/live/{SITE_CODE.lower()}/handoff")
-    CORE_FILES.append(f"./input/project_download/data_store/live/{SITE_CODE.lower()}/sms_content_map")
-    CORE_FILES.append("./input/project_download/agent_configuration/domain/variants.yaml")
-    CORE_FILES.append("./input/project_download/agent_configuration/domain/utterances.en-US.yaml")
-
-    # setup logging
-    logger = logging.getLogger("EVASnapshot v2")
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    handler = SysLogHandler(address=(PAPERTRAIL[0], PAPERTRAIL[1]))
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    
-    logger.info(f"CUSTOMER_EMAIL: {CUSTOMER_EMAIL}, SITE_CODE: {SITE_CODE}, ACCOUNT ID: {ACCOUNT_ID}, PROJECT ID: {PROJECT_ID}")
-   
-    main()
-    print("\nThe End.")
