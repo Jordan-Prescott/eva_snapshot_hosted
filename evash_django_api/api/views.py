88b0ee9342f6dd61d9ce3727310cc126787dcc13
Jordan Prescott <jprescott23@gmail.com>
2023-11-06 11:06:59 +0000

    Authentication complete

--- evash_django_api/api/views.py
+++ evash_django_api/api/views.py
@@ -39,8 +39,6 @@ def eva_snapshot(request):
     """ ...
     """
     
-    return Response({"worked"})
-    
     # this will return a zip file with snapshot output.
     account_id = request.GET.get('account_id', None)
     project_id = request.GET.get('project_id', None)
@@ -50,17 +48,15 @@ def eva_snapshot(request):
 
     # check params
     if account_id == None or account_id == '':
-        return HttpResponse('Error: no account_id specified')
+        return Response({"Error": "no account_id specified"}, status=status.HTTP_400_BAD_REQUEST)
     elif project_id == None or project_id == '':
-        return HttpResponse('Error: no project_id specified')
+        return HttpResponse({"Error": "no project_id specified"}, status=status.HTTP_400_BAD_REQUEST)
     elif group_id == None or group_id == '':
-            return HttpResponse('Error: no group_id specified')
+            return HttpResponse({"Error": "no group_id specified"}, status=status.HTTP_400_BAD_REQUEST)
     elif region == None or region == '':
-            return HttpResponse('Error: no region specified')
+            return HttpResponse({"Error": "no region specified"}, status=status.HTTP_400_BAD_REQUEST)
     elif customer_email == None or customer_email == '':
-            return HttpResponse('Error: no customer_email specified')
-    
-    #TODO: add auth here
+            return HttpResponse({"Error": "no customer_email specified"}, status=status.HTTP_400_BAD_REQUEST)
 
     
     # run eva snapshot script
